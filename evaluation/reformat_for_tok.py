2ff738f264306c7aa893fcf7052d9f80c4ee108c
pgergo <pagstudium@gmail.com>
2021-11-23 23:16:47 +0100

    [fix] evaluation inconsistencies and bug

--- evaluation/reformat_for_tok.py
+++ evaluation/reformat_for_tok.py
@@ -9,8 +9,8 @@ for line in sys.stdin:
         sys.stdout.write(line)
     else:
         t = re.sub(r'(\\|\|)(\d)(\S)', r'\1\2 \3', cols[1])
-        t = re.sub(r'(\\|\|)(\d)', r'\1\2.', cols[1])
-        
+        t = re.sub(r'(\\|\|)(\d)', r'\1\2.', t)
+
         m = re.findall(r'\\([0-9]).', t)
         for original_index in m:
             if original_index == '0':
@@ -18,6 +18,6 @@ for line in sys.stdin:
             t = t.replace(f'\\{original_index}.', f'\\{index_counter}').\
                     replace(f'|{original_index}.', f'|{index_counter}')
             index_counter += 1
-        
+
         t = t.replace('\\0.','_p0')
         print(t.replace('\\', '_p').replace('|', '_v'))
